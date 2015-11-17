b9a0b2d23a005779fa9bf939d29bcf5b93a29d1b
Alex_P <pekler-sasha@yandex.ru>
2015-11-17 20:40:03 +0500

    Adding Group id parser

--- app/src/main/java/ru/alexp/itschool/vkimages/ChooseGroupActivity.java
+++ app/src/main/java/ru/alexp/itschool/vkimages/ChooseGroupActivity.java
@@ -1,11 +1,12 @@
 package ru.alexp.itschool.vkimages;
 
-import android.renderscript.Sampler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ChooseGroupActivity extends AppCompatActivity {
 
@@ -14,12 +15,54 @@ public class ChooseGroupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_group);
 
-        ((Button) findViewById(R.id.selectGroupButton)).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.selectGroupButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String value = ((EditText) findViewById(R.id.groupIdField)).getText().toString();
-                Vars.putValue("groupId", value);
+                String id = parseGroupId(((EditText) findViewById(R.id.groupIdField)).getText().toString());
+                if (id != null) {
+                    Vars.putValue("groupId", id);
+                    setContentView(R.layout.activity_image_weaver);
+                } else {
+                    ((TextView) findViewById(R.id.infoMessage)).setText("NotFound");
+                }
+
             }
         });
+
+        ((EditText) findViewById(R.id.groupIdField)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                update();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                update();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                update();
+            }
+
+            private void update() {
+                ((TextView) findViewById(R.id.infoMessage)).setText(""); // при изменении очищаем лэйбл с ошибкой
+            }
+        });
+    }
+
+    private String parseGroupId(String id) {
+        if (id.matches("^(https?://vk\\.com/)?([^/]+)$")) {
+
+            id = id.replaceAll("^(https?://vk\\.com/)?([^/]+)/?$", "$2"); // если нам выдали целый URL
+
+            if (id.matches("^(-|public)?(\\d{7,9})$")) { // если нам выдали id группы
+                return '-' + id.replaceAll("^(-|public)?(\\d{7,9})$", "$2");
+            } else {
+                return id;
+            }
+        } else {
+            return null; // если всё очень плохо
+        }
     }
 }
